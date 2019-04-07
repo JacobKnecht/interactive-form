15e75050a42a772908dc1609a9ac99ef645848e5
Jacob Knecht <jacobknecht3@gmail.com>
2019-04-07 18:02:15 -0500

    added code to script.js to prevent users from scheduling conflicting events and to display their total cost for attending the conference

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 //alert("index.html is linked to script.js");
+//global variable declarations
+let total_cost = 0;
 
 //event handler for when the page loads
 $(document).ready(() => {
@@ -8,8 +10,7 @@ $(document).ready(() => {
   $('#other-title').hide();
 });
 
-//value="js puns" colors: cornflowerblue, darkslategrey, gold
-//value="heart js" colors: tomato, steelblue, dimgrey
+//"T-Shirts info" Section
 //'change' event handler on the design menu to change the colors displayed in
 //the color menu to match the selected design
 $('#design').on('change', (e) => {
@@ -61,5 +62,64 @@ $('#design').on('change', (e) => {
     $('#color option').each(function() {
       $(this).show();
     });
+  }
+});
+
+//"Register for Activites" section
+/*
+  Shared Times:
+  Tuesday 9am-12pm: JavaScript Frameworks Workshop, Express Workshop
+  Tuesday 1pm-4pm: JavaScript Libraries Workshop, Node.js Workshop
+  Wednesday 9am-12pm: Build Tools Workshop
+  Wednesday 1pm-4pm: npm Workshop
+*/
+//select the 'activities' fieldset
+$('.activities input').on('change', function() {
+  //variable declarations
+  const regex = /\d{3}/;
+  const label_text = $(this).closest('label').text();
+  const cost = label_text.match(regex);
+  //if change causes check
+  if($(this).is(':checked')) {
+    //check for conflicting times, if necessary disable checkboxes
+    //and mark out the text, add the price to the running total
+    if($(this).attr('name') === 'js-frameworks') {
+      $('input[name="express"]').attr('disabled', true)
+        .closest('label').css('textDecoration', 'line-through');
+    } else if($(this).attr('name') === 'express') {
+      $('input[name="js-frameworks"]').attr('disabled', true)
+        .closest('label').css('textDecoration', 'line-through');
+    } else if($(this).attr('name') === 'js-libs') {
+      $('input[name="node"]').attr('disabled', true)
+        .closest('label').css('textDecoration', 'line-through');
+    } else if($(this).attr('name') === 'node') {
+      $('input[name="js-libs"]').attr('disabled', true)
+        .closest('label').css('textDecoration', 'line-through');
+    }
+    total_cost += parseInt(cost[0]);
+  } else { //if change causes uncheck
+    //check for conflicting times, if necessary enable checkboxes and turn
+    //text color back to black
+    if($(this).attr('name') === 'js-frameworks') {
+      $('input[name="express"]').removeAttr('disabled')
+        .closest('label').css('textDecoration', 'none');
+    } else if($(this).attr('name') === 'express') {
+      $('input[name="js-frameworks"]').removeAttr('disabled')
+        .closest('label').css('textDecoration', 'none');
+    } else if($(this).attr('name') === 'js-libs') {
+      $('input[name="node"]').removeAttr('disabled')
+        .closest('label').css('textDecoration', 'none');
+    } else if($(this).attr('name') === 'node') {
+      $('input[name="js-libs"]').removeAttr('disabled')
+        .closest('label').css('textDecoration', 'none');
+    }
+    total_cost -= parseInt(cost[0]);
+  }
+  //generate total cost
+  if(total_cost > 0) {
+    $('.activities').append('<label id="total_cost"></label>');
+    $('#total_cost').text(`Total: \$${total_cost}`)
+  } else {
+    $('#total_cost').remove();
   }
 });
